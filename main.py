830058e9cb9bd491177b6ee96b7af3349849cdd1
cardo1011 <ricardomunoz4250@gmail.com>
2025-07-21 19:38:06 -0700

    the sleep functions with the WebDriverWait method from selenium because it seemed more realistic and reasonable to maintain with time.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service 
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -13,11 +16,25 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 driver.get("https://www.ea.com/ea-sports-fc/ultimate-team/web-app/")
 driver.maximize_window()
 
-time.sleep(4)
+# wait for the "Login" button to load 
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//button[text()='Login']"))
+    )
+    print("Reached login page!")
+except:
+    print("Couldn't reach login page :(")
 
-buttons = driver.find_elements("xpath", "//button")
+# click Login button once it loads
+buttons = driver.find_elements("xpath", "//button[text()='Login']")
 buttons[0].click()
 
-
-
+# wait for user to login into FUT web app
+try:
+    element = WebDriverWait(driver, 120).until(
+        EC.presence_of_element_located((By.XPATH, "//button[text()='Login']"))
+    )
+    print("Reached home page!")
+except:
+    print("Timed out waiting for the home page to load...")
 
