411f3348ff79ccbb0da7cfdda5d264c5dbd78c2a
cardo1011 <ricardomunoz4250@gmail.com>
2025-07-22 21:01:11 -0700

    set up the xpaths and the clicking of the store icon and the unassigned tile to get to the packs.

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 driver.get("https://www.ea.com/ea-sports-fc/ultimate-team/web-app/")
 driver.maximize_window()
 
-# wait for the "Login" button to load 
+# wait for the "Login" button to load and be clickable
 try:
     element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//button[text()='Login']"))
+        EC.element_to_be_clickable((By.XPATH, "//button[text()='Login']"))
     )
     print("Reached login page!")
 except:
@@ -29,12 +29,27 @@ except:
 buttons = driver.find_elements("xpath", "//button[text()='Login']")
 buttons[0].click()
 
-# wait for user to login into FUT web app
+# waits at most 120 seconds for user to login into FUT web app before moving on with the script
 try:
-    element = WebDriverWait(driver, 120).until(
-        EC.presence_of_element_located((By.XPATH, "//button[text()='Login']"))
+    element = WebDriverWait(driver, 180).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'title'))
     )
     print("Reached home page!")
 except:
     print("Timed out waiting for the home page to load...")
 
+# finds all the button elements that are children of a nav element
+home_buttons = driver.find_elements(By.XPATH, "//nav/button")
+
+# iterate through the elements of home_buttons to find the "Store" button and click on it when found 
+for button in home_buttons:
+    if "Store" in button.get_attribute('innerHTML'):
+        button.click()
+        print("Store button clicked")
+
+# Wait for the store to load and give us the tile that shows if there is any "Unassigned items"
+items_tile = driver.find_elements(By.XPATH,  "//div[contains(@class, 'ut-unassigned-tile-view') and contains(@class, 'tile')]")
+# wait for the "unassigned items tile to appear"
+time.sleep(5)
+# clicks on the unassigned items tile
+items_tile[0].click()
