14100184bc9125d3a6a08f806c1d752f2d49c27c
cardo1011 <ricardomunoz4250@gmail.com>
2025-07-31 16:04:46 -0700

    refactored the first click on the Packs tile to be wrapped by a try block

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def button_to_be_clicked(list_of_buttons: list, innerHTML_of_button: str):
     return False
 
 options = Options()
-options.add_experimental_option("detach", True)
+options.add_experimental_option("detach", True) 
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
@@ -32,9 +32,9 @@ try:
     element = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, "//button[text()='Login']"))
     )
-    print("Reached login page!")
+    print("\nReached login page!\n")
 except:
-    print("Couldn't reach login page :(")
+    print("\nCouldn't reach login page :(\n")
 
 # click Login button once it loads
 buttons = driver.find_elements("xpath", "//button[text()='Login']")
@@ -45,77 +45,79 @@ try:
     element = WebDriverWait(driver, 180).until(
         EC.presence_of_element_located((By.CLASS_NAME, 'title'))
     )
-    print("Reached home page!")
+    print("\nReached home page!\n")
 except:
-    print("Timed out waiting for the home page to load...")
+    print("\nTimed out waiting for the home page to load...\n")
 
 # finds all the button elements on the home page that are children of a nav element and clicks on the button with the text that contains "Store"
 home_buttons = findButtons('//nav/button')
 button_to_be_clicked(home_buttons, "Store")
 
 # Wait for the store to load and then clicks on the "Packs" tile
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'packs-tile') and contains(@class, 'tile')]")))
-driver.find_element(By.XPATH, "//div[contains(@class, 'packs-tile') and contains(@class, 'tile')]").click()
+try:
+    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'packs-tile') and contains(@class, 'tile')]")))
+    driver.find_element(By.XPATH, "//div[contains(@class, 'packs-tile') and contains(@class, 'tile')]").click()
+    print("\nClicked on \"Packs\" tile\n")
+except:
+    print("\nTimed out waiting for packs tile to load\n")
 
 time.sleep(3)
 packs = findButtons("//button[contains(@class, 'currency') and contains(@class, 'call-to-action')]")
 
-while packs:
-    # opens pack
-    packs[0].click()
+# while packs: #commented out to test code while refactoring
+# opens pack
+packs[0].click()
 
-    time.sleep(10)
+time.sleep(10)
 
-    # quick sell all contents of pack that are under the H2 that contains the string "Duplicates"
-    if driver.find_elements(By.XPATH, "//h2[contains(normalize-space(), 'Duplicates')]"):
-        ellipsis_btn = driver.find_element(By.XPATH, "//h2[contains(text(), 'Duplicates')]/ancestor::header//button[contains(@class, 'ellipsis-btn')]")
-        ellipsis_btn.click()
-
-
-        quick_sell_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[.//span[@class='btn-text currency-coins' and contains(text(), 'Quick Sell')]]")))
-        quick_sell_button.click()
-
-        time.sleep(2)
-        ok_btns = findButtons('//button')
-        button_to_be_clicked(ok_btns, "Ok")
-
-    if driver.find_elements(By.XPATH, "//button[contains(@class, 'ellipsis-btn')]"):
-
-        # clicks on ellipsis button in the pack to handle the contents of the pack
-        ellipsis_btn = driver.find_element(By.XPATH, "//button[contains(@class, 'ut-image-button-control') and contains(@class, 'ellipsis-btn')]")
-
-        ellipsis_btn.click()
-
-        time.sleep(3)
-        #clicks button to store all contents of 
-        store_in_club_btns = findButtons('//button')
-        button_to_be_clicked(store_in_club_btns, "Store All in Club")
+# quick sell all contents of pack that are under the H2 that contains the string "Duplicates"
+if driver.find_elements(By.XPATH, "//h2[contains(normalize-space(), 'Duplicates')]"):
+    ellipsis_btn = driver.find_element(By.XPATH, "//h2[contains(text(), 'Duplicates')]/ancestor::header//button[contains(@class, 'ellipsis-btn')]")
+    ellipsis_btn.click()
 
 
+    quick_sell_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//button[.//span[@class='btn-text currency-coins' and contains(text(), 'Quick Sell')]]")))
+    quick_sell_button.click()
+
+    time.sleep(2)
+    ok_btns = findButtons('//button')
+    button_to_be_clicked(ok_btns, "Ok")
+
+if driver.find_elements(By.XPATH, "//button[contains(@class, 'ellipsis-btn')]"):
+
+    # clicks on ellipsis button in the pack to handle the contents of the pack
+    ellipsis_btn = driver.find_element(By.XPATH, "//button[contains(@class, 'ut-image-button-control') and contains(@class, 'ellipsis-btn')]")
+
+    ellipsis_btn.click()
 
     time.sleep(3)
-    pack_items_cleared = driver.find_elements(By.XPATH, "//h2[normalize-space()='You have no unassigned items.']")
+    #clicks button to store all contents of 
+    store_in_club_btns = findButtons('//button')
+    button_to_be_clicked(store_in_club_btns, "Store All in Club")
 
-    if pack_items_cleared:
-        home_buttons = findButtons('//nav/button')
-        button_to_be_clicked(home_buttons, "Store")
 
-    # ++++++++++++++++++++++++++++++++++++++ Last thing i did was refactor the waiting for the "Packs" tile to be visible before clicking on it again
 
-    # Wait for the store to load and then clicks on the "Packs" tile
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'packs-tile') and contains(@class, 'tile')]")))
-    driver.find_element(By.XPATH, "//div[contains(@class, 'packs-tile') and contains(@class, 'tile')]").click()
-      
-    # update packs to reflect the new amount of packs to be opened before the next iteration condition check 
-    try:
-        WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//button[contains(@class, 'currency') and contains(@class, 'call-to-action')]"))
-        )
-    except:
-        print("No more packs found. Ending loop.")
-        break
+time.sleep(3)
+pack_items_cleared = driver.find_elements(By.XPATH, "//h2[normalize-space()='You have no unassigned items.']")
 
-    packs = findButtons("//button[contains(@class, 'currency') and contains(@class, 'call-to-action')]")
+if pack_items_cleared:
+    home_buttons = findButtons('//nav/button')
+    button_to_be_clicked(home_buttons, "Store")
+
+# Wait for the store to load and then clicks on the "Packs" tile
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'packs-tile') and contains(@class, 'tile')]")))
+driver.find_element(By.XPATH, "//div[contains(@class, 'packs-tile') and contains(@class, 'tile')]").click()
+    
+# update packs to reflect the new amount of packs to be opened before the next iteration condition check 
+try:
+    WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//button[contains(@class, 'currency') and contains(@class, 'call-to-action')]"))
+    )
+except:
+    print("No more packs found. Ending loop.")
+    # break
+
+packs = findButtons("//button[contains(@class, 'currency') and contains(@class, 'call-to-action')]")
 
 
